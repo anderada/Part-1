beaf1b2391d3a360aa8e70c567cd304f68fbe3d1
anderada <anderada@sheridancollege.ca>
2024-01-11 10:23:09 -0500

    Task 2 b completed: tank firing a missile on kayboard input.

--- Assets/Week 1/Scripts/Mover.cs
+++ Assets/Week 1/Scripts/Mover.cs
@@ -5,11 +5,17 @@ using UnityEngine;
 public class Mover : MonoBehaviour
 {
     float speed = 5f;
+    public GameObject prefab;
+    public Transform barrel;
 
     // Update is called once per frame
     void Update()
     {
         float keyboardInput = Input.GetAxis("Horizontal");
         transform.Translate(keyboardInput * speed * Time.deltaTime, 0, 0);
+
+        if (Input.GetKeyDown(KeyCode.Space)) {
+            Instantiate(prefab, barrel.position, barrel.rotation);
+        }
     }
 }
