647232785ce759b2b0edc501211dbb7f147c1ab5
anderada <anderada@sheridancollege.ca>
2024-01-11 10:37:24 -0500

    Task 4 a completed: missile moves when fired.

---
+++ Assets/Missile.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+public class Missile : MonoBehaviour
+{
+    public float speed = 20f;
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(speed * Time.deltaTime, 0, 0);
+    }
+}
